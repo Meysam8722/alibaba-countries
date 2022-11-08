37dfb7aa56d7971c4af399a6d9fb7509ea3ca006
Meysam Shariatmadary <meysamshariatmadary@gmail.com>
2022-11-08 10:31:08 +0330

    added region filter

--- components/Countries.vue
+++ components/Countries.vue
@@ -1,68 +1,80 @@
 <template>
-<v-container fluid>
-  <v-row>
-    <v-col v-for="item in searchedCountry" :key="item.numericCode" cols="12" sm="3">
-      <v-card max-width="200" height="400" class="mx-0">
-        <v-img
-          height="100"
-          :src="item.flag"
-        />
-        <v-card-title>
-          {{item.name}}
-        </v-card-title>
-        <v-card-text>
-          <div class="normal-size">
-            <span><strong>Population:</strong> {{item.population}}<br></span>
-            <span><strong>Region:</strong> {{item.region}}<br></span>
-            <span><strong>Capital:</strong> {{item.capital}}<br></span>
-          </div>
-        </v-card-text>
-      </v-card>
-    </v-col>
-  </v-row>
-</v-container>
+  <v-container fluid>
+    <v-row>
+      <v-col
+        v-for="item in filteredCountries"
+        :key="item.numericCode"
+        cols="12"
+        sm="3"
+      >
+        <v-card max-width="200" height="400" class="mx-0">
+          <v-img height="100" :src="item.flag" />
+          <v-card-title>
+            {{ item.name }}
+          </v-card-title>
+          <v-card-text>
+            <div class="normal-size">
+              <span
+                ><strong>Population:</strong> {{ item.population }}<br
+              /></span>
+              <span><strong>Region:</strong> {{ item.region }}<br /></span>
+              <span><strong>Capital:</strong> {{ item.capital }}<br /></span>
+            </div>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 <script>
 export default {
-  name: 'Countries',
+  name: "Countries",
   props: {
     search: {
       type: String,
-      default: () => '',
+      default: () => "",
+    },
+    region: {
+      type: String,
+      default: () => "",
     },
   },
   data() {
     return {
       items: [],
-    }
+    };
   },
   async fetch() {
     const config = {
-      method: 'get',
-      url: 'https://restcountries.com/v2/all',
-    }
+      method: "get",
+      url: "https://restcountries.com/v2/all",
+    };
     await this.$axios(config)
-    .then((response) => {
-      console.log(response.data)
-      this.items = response.data
-    })
-    .catch((error) => {
-      console.log(error.response)
-    })
+      .then((response) => {
+        console.log(response.data);
+        this.items = response.data;
+      })
+      .catch((error) => {
+        console.log(error.response);
+      });
   },
   computed: {
-    searchedCountry() {
-      if(this.search === ''){
-        return this.items
-      }else{
+    filteredCountries() {
+      if (this.region === "") {
         return this.items.filter((item) => {
-          return item.name.toLowerCase().includes(this.search.toLowerCase())
-        })
+          return item.name.toLowerCase().includes(this.search.toLowerCase());
+        });
+      } else {
+        return this.items.filter((item) => {
+          return (
+            item.region.toLowerCase() === this.region.toLowerCase() &&
+            item.name.toLowerCase().includes(this.search.toLowerCase())
+          );
+        });
       }
-
     },
   },
-}
+};
 </script>
 <style scoped>
 .normal-size {
--- pages/index.vue
+++ pages/index.vue
@@ -1,49 +1,95 @@
 <template>
   <v-container fluid class="pa-0">
-    <div class="d-flex flex-row justify-space-between align-center">
+    <div
+      class="d-flex flex-row justify-space-between align-center pa-4 full-width"
+    >
       <v-text-field
         v-model="search"
         placeholder="Search for a country..."
         prepend-inner-icon="mdi-magnify"
         loading="false"
+        single-line
+        solo
+        dense
+        hide-details
+        max-width="100px"
       ></v-text-field>
+      <v-select
+        v-model="region"
+        :items="menuItems"
+        item-text="name"
+        item-value="id"
+        placeholder="Filter by Region"
+        :menu-props="{ bottom: true, offsetY: true }"
+        persistent-hint
+        return-object
+        solo
+        dense
+        single-line
+        hide-details
+      ></v-select>
     </div>
-    <Countries :search="search"/>
-    <TabdealSlider/>
-    <CoinList/>
-    <TabdealImages/>
-    <TabdealFamily/>
-    <WhyTabdeal/>
-    <TabdealAvailablity/>
+    <Countries :search="search" :region="region.name" />
+    <TabdealSlider />
+    <CoinList />
+    <TabdealImages />
+    <TabdealFamily />
+    <WhyTabdeal />
+    <TabdealAvailablity />
   </v-container>
 </template>
 <script>
-  import TabdealSlider from "@/components/TabdealSlider";
-  import CoinList from "@/components/CoinList";
-  import TabdealImages from "@/components/TabdealImages";
-  import TabdealFamily from "@/components/TabdealFamily";
-  import WhyTabdeal from "@/components/WhyTabdeal";
-  import TabdealAvailablity from "@/components/TabdealAvailablity";
-  import Countries from '~/components/Countries'
+import TabdealSlider from "@/components/TabdealSlider";
+import CoinList from "@/components/CoinList";
+import TabdealImages from "@/components/TabdealImages";
+import TabdealFamily from "@/components/TabdealFamily";
+import WhyTabdeal from "@/components/WhyTabdeal";
+import TabdealAvailablity from "@/components/TabdealAvailablity";
+import Countries from "~/components/Countries";
 
-  export default {
-    name: 'HomePage',
-    data() {
-      return{
-        search: '',
-      }
-    },
-    components: {
-      TabdealSlider,
-      CoinList,
-      TabdealImages,
-      TabdealFamily,
-      WhyTabdeal,
-      TabdealAvailablity,
-      Countries,
-    }
-  }
+export default {
+  name: "HomePage",
+  data() {
+    return {
+      search: "",
+      region: "",
+      menuItems: [
+        {
+          id: 1,
+          name: "Africa",
+        },
+        {
+          id: 2,
+          name: "Americas",
+        },
+        {
+          id: 3,
+          name: "Asia",
+        },
+        {
+          id: 4,
+          name: "Europe",
+        },
+        {
+          id: 5,
+          name: "Oceania",
+        },
+      ],
+    };
+  },
+  components: {
+    TabdealSlider,
+    CoinList,
+    TabdealImages,
+    TabdealFamily,
+    WhyTabdeal,
+    TabdealAvailablity,
+    Countries,
+  },
+};
 </script>
 <style scoped>
-
+>>> .v-input__slot {
+  max-width: 250px !important;
+}
 </style>
